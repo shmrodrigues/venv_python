f5543a0eaa3e6ec732b48fd59f6d429cf5e59d46
Sergio <shmrodrigues@gmail.com>
2025-03-08 20:37:55 -0300

    Primeiro commit do Projeto

---
+++ Cria_venc.py
@@ -0,0 +1,23 @@
+print("Criar ambiente virtual")
+
+# Cria um ambiente virtual chamado .venv na pasta atual.
+# Na pasta .venv, é criado um ambiente virtual para o projeto todas as bibliotecas instaladas ficam dentro da pasta .venv
+# python -m venv .venv
+
+# Ativa o ambiente virtual.
+# .venv\Scripts\activate - No windows
+# source .venv/bin/activate - No Linux/MacOs
+
+#pip list
+#pip freeze
+
+#Gererar um arquivo de dependências do projeto para instalar as bibliotecas necessárias.
+#pip freeze > requirements.txt
+
+#pip install jupyter notebook
+
+# Atualizar as bibliotecas do projeto.
+#pip freeze > requirements.txt
+
+#Instalar as bilbiotecas do projeto.
+#pip install -r requirements.txt
